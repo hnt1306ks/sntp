038cb66e2290eea2ab4bd23b328f9bc8a3399fd9
hnt1306ks <108657043+hnt1306ks@users.noreply.github.com>
2022-07-06 21:21:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'to: Phương Thùy';
+const giftUrl = 'https://www.facebook.com/trthuyphuong';
+const eventName = 'from: Hưng IT :))) ';
+const titleCard = 'Happy Birthday ';
+const contentCard = 'chúc mừng sinh nhật bạn Thùy Phương, chúc bạn tuổi mới mau ăn chóng lớn, ngày càng học giỏi và ngày càng xinh đẹp nhé :33';
+const giftImage = 'tp.png';
+//const base64 = '';
+//const giftImageBase64 = "data:image/png;base64, " + base64;
